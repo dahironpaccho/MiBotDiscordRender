a47ae66aed3591e6a7ed79183861120cf0854435
dahironpaccho <dahironpaccho@gmail.com>
2025-06-03 20:51:27 -0500

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -6,10 +6,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "El bot está vivo 😎"
+    return "Estoy vivo!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
 
 def keep_alive():
     t = Thread(target=run)
